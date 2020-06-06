bd049e3bef263b330341624eec49f2bc04b7d6d9
HenryKenya <henry.onyango@outlook.com>
2020-06-06 18:02:06 +0300

    Configure pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {PostsModule} from './components/posts/posts.module';
 import {ServicesModule} from './services/services.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import {ServicesModule} from './services/services.module';
   imports: [
     BrowserModule,
     PostsModule,
-    ServicesModule
+    ServicesModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
